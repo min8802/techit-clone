880629ee6948c79b15201a80f5ef7cba6bc3a030
min8802 <ww330@naver.com>
2024-05-17 11:34:53 +0900

    TopBanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import TopBanner from "./components/TopBanner";
+import { useEffect } from "react";
+
 const App = () => {
-  return <div className="bg-red-100">Hi </div>
+  const [isViewed, setIsViewed] = useState(false);
+
+  useEffect(() => {
+    const savedIsViewed = localStorage.getItem("isViewed");
+    
+    if(savedIsViewed === "1") {
+      setIsViewed(true);
+    }
+  }, []);
+
+  return (
+    <div className="font-pretendard">
+      {!isViewed && <TopBanner setIsViewed={setIsViewed}/>}
+    </div>
+  );
 };
 
 export default App;
